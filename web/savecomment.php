70b3aa16645165f173aff2d6d0286b98d9e30ea4
gs <gs@38eb2766-93bc-944b-8ed2-646c695adf1f>
2008-09-30 13:38:53 +0000

    开始建立网页后台的结构，写好了数据库类的基本函数 修改了目录结构
    git-svn-id: https://dns.bbxy.net:8443/svn/daikazoku@38 38eb2766-93bc-944b-8ed2-646c695adf1f

--- web/savecomment.php
+++ web/savecomment.php
@@ -1,14 +1,13 @@
 <?php
-require('inc/db_dm.php');
+require_once("类文件/头.php");
 
-$数据库 = new 数据库达梦类;
+require_once('类文件/邀踢动画类.php');
 
-$数据库->主机 = 'localhost';
-$数据库->用户名 = '邀踢动画';
-$数据库->用户名 = mb_convert_encoding($数据库->用户名, 'utf-8');
-echo $数据库->用户名;
-$数据库->密码 = 'ydmzfnfsosftd12h';
-//$数据库->密码 = 'SYSDBA';
-$数据库->模式 = '邀踢动画';
+$邀踢动画 = new 邀踢动画类;
+
+
+
 $数据库->连接();
+
 ?>
+
