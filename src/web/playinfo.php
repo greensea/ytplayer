8baaf6d98cd1bfa02c6afdb3f579510e257bbeb8
greensea <gs@bbxy.net>
2013-08-03 20:19:53 +0800

    playinfo.php 增加 json 的输出支持

--- src/web/playinfo.php
+++ src/web/playinfo.php
@@ -1,15 +1,9 @@
 <?php
 require_once('require/header.php');
 
-$可扩展标记语言。评论元素 = '';
-$可扩展标记语言 = '';
 $动画数组 = $弹幕数组 = null;
 
 //载入模板
-$可扩展标记语言模板。评论元素 = ytp_file_get_contents('模板/闪弹幕数据。评论元素.xml');
-$可扩展标记语言模板。评论元素。通道属性 = ytp_file_get_contents('模板/闪弹幕数据。评论元素。通道属性.xml');
-$可扩展标记语言模板。评论元素。位置属性 = ytp_file_get_contents('模板/闪弹幕数据。评论元素。位置属性.xml');
-$可扩展标记语言模板 = ytp_file_get_contents('模板/闪弹幕数据.xml');
 
 //获取动画数据
 $是否重定位 = isset($_GET['relocate']) ? ($_GET['relocate'] == 1) : false;
@@ -63,66 +57,122 @@ if($是否重定位){
 	exit();
 }
 
-//构建弹幕数组的XML
-for($i = 0; $i < count($弹幕数组); $i++){
-	if ($弹幕数组[$i]['id'] == 0) break;
-	//$弹幕数组[$i]['评论时间'] = strtotime($弹幕数组[$i]['评论时间']);
-	$评论时间 = $弹幕数组[$i]['popsubtime'];
-	$速度 = $弹幕数组[$i]['speed'];
-	$字号 = $弹幕数组[$i]['fontsize'];
-	$模式 = $弹幕数组[$i]['flymode'];
-	$通道 = $弹幕数组[$i]['channel'];
-	$位置 = $弹幕数组[$i]['position'];
-	$是否字幕 = false;
-	switch($模式){
-		case FLY_MODE_FLY:
-			$模式 = 'fly';
-			break;
-		case FLY_MODE_TOP:
-			$模式 = 'top';
-			break;
-		case FLY_MODE_BOTTOM:
-			$模式 = 'bottom';
-			break;
-		case FLY_MODE_SUBTITLE:
-			$是否字幕 = true;
-			$模式 = 'bottom';
-			break;
-		default:
-			$模式 = 'fly';
-			break;
+function 可扩展标记语言输出($弹幕数组) {
+	global $动画编号;
+	global $影片地址;
+	global $播放次数;
+	global $标题;
+	global $说明;
+	
+	$可扩展标记语言。评论元素 = '';
+	$可扩展标记语言 = '';
+
+	$可扩展标记语言模板。评论元素 = ytp_file_get_contents('模板/闪弹幕数据。评论元素.xml');
+	$可扩展标记语言模板。评论元素。通道属性 = ytp_file_get_contents('模板/闪弹幕数据。评论元素。通道属性.xml');
+	$可扩展标记语言模板。评论元素。位置属性 = ytp_file_get_contents('模板/闪弹幕数据。评论元素。位置属性.xml');
+	$可扩展标记语言模板 = ytp_file_get_contents('模板/闪弹幕数据.xml');
+
+	//构建弹幕数组的XML
+	for($i = 0; $i < count($弹幕数组); $i++){
+		if ($弹幕数组[$i]['id'] == 0) break;
+		//$弹幕数组[$i]['评论时间'] = strtotime($弹幕数组[$i]['评论时间']);
+		$评论时间 = $弹幕数组[$i]['popsubtime'];
+		$速度 = $弹幕数组[$i]['speed'];
+		$字号 = $弹幕数组[$i]['fontsize'];
+		$模式 = $弹幕数组[$i]['flymode'];
+		$通道 = $弹幕数组[$i]['channel'];
+		$位置 = $弹幕数组[$i]['position'];
+		$是否字幕 = false;
+		switch($模式){
+			case FLY_MODE_FLY:
+				$模式 = 'fly';
+				break;
+			case FLY_MODE_TOP:
+				$模式 = 'top';
+				break;
+			case FLY_MODE_BOTTOM:
+				$模式 = 'bottom';
+				break;
+			case FLY_MODE_SUBTITLE:
+				$是否字幕 = true;
+				$模式 = 'bottom';
+				break;
+			default:
+				$模式 = 'fly';
+				break;
+		}
+
+		$可扩展标记语言。评论元素 .= sprintf($可扩展标记语言模板。评论元素,
+																$弹幕数组[$i]['id'],
+																$字号,
+																$速度,
+																$弹幕数组[$i]['color'],
+																$模式,
+																$是否字幕,
+																$弹幕数组[$i]['playtime'] / 1000,
+																$评论时间,
+																($通道 == null) ? '' : sprintf($可扩展标记语言模板。评论元素。通道属性, $通道),
+																($位置 == null) ? '' : sprintf($可扩展标记语言模板。评论元素。位置属性, $位置),
+																htmlspecialchars($弹幕数组[$i]['content'])
+													);
+																
 	}
 
-	$可扩展标记语言。评论元素 .= sprintf($可扩展标记语言模板。评论元素,
-															$弹幕数组[$i]['id'],
-															$字号,
-															$速度,
-															$弹幕数组[$i]['color'],
-															$模式,
-															$是否字幕,
-															$弹幕数组[$i]['playtime'] / 1000,
-															$评论时间,
-															($通道 == null) ? '' : sprintf($可扩展标记语言模板。评论元素。通道属性, $通道),
-															($位置 == null) ? '' : sprintf($可扩展标记语言模板。评论元素。位置属性, $位置),
-															htmlspecialchars($弹幕数组[$i]['content'])
-												);
-															
+	$可扩展标记语言 = sprintf($可扩展标记语言模板, 
+											$动画编号,
+											htmlspecialchars($影片地址),
+											$播放次数,
+											htmlspecialchars($标题),
+											htmlspecialchars($说明),
+											$可扩展标记语言。评论元素
+										);
+										
+	return $可扩展标记语言;
 }
 
-$可扩展标记语言 = sprintf($可扩展标记语言模板, 
-										$动画编号,
-										htmlspecialchars($影片地址),
-										$播放次数,
-										htmlspecialchars($标题),
-										htmlspecialchars($说明),
-										$可扩展标记语言。评论元素
-									);
+function 爪哇脚本对象表示($弹幕数组) {
+	global $动画编号;
+	global $影片地址;
+	global $播放次数;
+	global $标题;
+	global $说明;
+	
+	/// 删除弹幕数组中以数字作为键的元素
+	for ($i = 0; $i < count($弹幕数组); $i++) {
+		$k = 0;
+		while (array_key_exists($k, $弹幕数组[$i])) {
+			unset($弹幕数组[$i][$k]);
+			$k++;
+		}
+	}
+	
+	$结果数组 = array();
+	
+	$结果数组['video']['flvID'] = $动画编号;
+	$结果数组['video']['flvURL'] = array();
+	foreach (explode('\n', $影片地址) as $键 => $值) {
+		array_push($结果数组['video']['flvURL'], $值);
+	}
+	$结果数组['video']['playTimes'] = $播放次数;
+	$结果数组['video']['title'] = $标题;
+	$结果数组['video']['description'] = $说明;
+	
+	$结果数组['comments'] = $弹幕数组;
+	
+	return json_encode($结果数组);
+}
 
 //echo htmlspecialchars($可扩展标记语言);
 ob_clean();
-header("Content-Type: text/xml; charset=utf-8"); 
-header("Cache-Control: no-cache, must-revalidate");
-echo $可扩展标记语言;
+
+if (isset($_GET['json'])) {
+	echo 爪哇脚本对象表示($弹幕数组);
+}
+else {
+	header("Content-Type: text/xml; charset=utf-8"); 
+	header("Cache-Control: no-cache, must-revalidate");
+	echo 可扩展标记语言输出($弹幕数组);
+}
 
 /*********************************************/
 
