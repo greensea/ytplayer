18042f36aa96631cf2e00d4fc8594be21c2d6300
greensea <gs@bbxy.net>
2013-08-26 13:45:34 +0800

    将 mysql 库更改为 mysqli

--- src/web/require/db_mysql.php
+++ src/web/require/db_mysql.php
@@ -3,13 +3,13 @@ require('db.php');
 
 class 数据库灭意思类 extends 数据库类{
 	private $连接对象;
-
+	
 	public function 连接(){
 		if($this->是否连接) return true;
 
-		mysql_connect('localhost', $this->用户名, $this->密码);
-		mysql_select_db($this->模式);
-		mysql_query('SET NAMES utf8');
+		$this->连接对象 = mysqli_connect('localhost', $this->用户名, $this->密码) or die(mysqli_error($this->连接对象));
+		mysqli_select_db($this->连接对象, $this->模式);
+		mysqli_query($this->连接对象, 'SET NAMES utf8');
 		
 		$this->是否连接 = true;
 	}
@@ -17,15 +17,15 @@ class 数据库灭意思类 extends 数据库类{
 	public function 查询($语句){
 		if(!$this->是否连接) $this->连接();
 
-		$结果 = mysql_query($语句) or die(mysql_error() . "<strong>$语句</strong>");
-		$this->影响行数 = @mysql_affected_rows($结果);
+		$结果 = mysqli_query($this->连接对象, $语句) or die(mysqli_error($this->连接对象) . "<strong>$语句</strong>");
+		$this->影响行数 = @mysqli_affected_rows($this->连接对象);
 		
 		//如果不是SELECT之类的语句就不用返回结果集了
-		if(@mysql_num_rows($结果) == null) return $this->影响行数;
+		if(@mysqli_num_rows($结果) == null) return $this->影响行数;
 
 		$返回 = array();
 
-		while($单行结果 = mysql_fetch_array($结果)){
+		while($单行结果 = mysqli_fetch_array($结果)){
 			array_push($返回, $单行结果);
 		}
 
@@ -33,7 +33,7 @@ class 数据库灭意思类 extends 数据库类{
 	}
 
 	public function 断开(){
-		mysql_close();
+		mysqli_close($this->连接对象);
 		$this->是否连接 = false;
 	}
 
